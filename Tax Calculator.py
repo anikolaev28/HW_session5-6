f8f1818acf1058dca41faa8bbc8c3fa4e91ed150
alejandro <anikolaev.ieu2023@student.ie.edu>
2025-01-23 16:16:13 +0100

    3rd

--- Tax Calculator.py
+++ Tax Calculator.py
@@ -10,14 +10,22 @@ while True:
             print("The gross salary must be positive")
     except ValueError:
             print("Invalid input. Please enter a valid number.")
-
-if gross <= 1000:
-    print(f"Your net salary is {gross - (gross * 0.10)}")
-elif gross <= 2000:
-    print(f"Your net salary is {gross - (gross * 0.12)}")
-elif gross <= 4000:
-    print(f"Your net salary is {gross - (gross * 0.14)}")
+while True:
+    try:
+        child = int(input("How many children do you have?"))
+        if child >= 0:
+            break
+        else:
+            print("The number of children must be positive")
+    except ValueError:
+            print("Invalid input. Please enter a valid number.")
+if gross < 1000:
+    print(f"Your net salary is {gross - (gross * (0.10-child*0.01))}")
+elif gross < 2000:
+    print(f"Your net salary is {gross - (gross * (0.12-child*0.01))}")
+elif gross < 4000:
+    print(f"Your net salary is {gross - (gross * (0.14-child*0.005))}")
 else:
-    print(f"Your net salary is {gross - (gross * 0.18)}")
+    print(f"Your net salary is {gross - (gross * (0.18-child*0.005))}")
 
 
